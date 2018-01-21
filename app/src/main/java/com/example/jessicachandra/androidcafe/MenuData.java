e6bf1556c7ed222537fedfee4b8135a0596a7620
jessicachandra <33944379+jessicachandra@users.noreply.github.com>
2018-01-21 20:04:52 +0800

    selesai

--- app/src/main/java/com/example/jessicachandra/androidcafe/MenuData.java
+++ app/src/main/java/com/example/jessicachandra/androidcafe/MenuData.java
@@ -25,12 +25,12 @@ public class MenuData {
     public static ArrayList<Menu> getListMenu() {
 
 
-        Log.v("aldo","10");
+
         String response = null;
         Menu menu = null;
 
 
-        Log.v("aldo","11");
+
         FetchMenuData conn = (FetchMenuData) new FetchMenuData();
         conn.execute();
         try {
@@ -42,40 +42,31 @@ public class MenuData {
         }
 
 
-        Log.v("aldo","12");
         ArrayList<Menu> list = new ArrayList<>();
 
 
-        Log.v("aldo","18");
+
         try {
 
 
-            Log.v("aldo","19");
+
             JSONArray jsonArray = new JSONArray(jsondata);
 
 
-            Log.v("aldo","14");
             Gson gson = new Gson();
             int i = 0;
 
 
-            Log.v("aldo","15");
             while (i < jsonArray.length()) {
                 list.add(gson.fromJson(jsonArray.getJSONObject(i).toString(), Menu.class));
                 i++;
             }
         } catch (JSONException e) {
 
-
-            Log.v("aldo","16");
             e.printStackTrace();
 
-
-            Log.v("aldo","17");
         }
 
-
-        Log.v("aldo","13");
         return list;
     }
     }
